cba97ec0c2cfac913ae2b25d5e7f5d0e260b758c
Emon Khalid <emonkhalid@gmail.com>
2018-04-10 10:18:30 +0600

    8-Commit Create posts with static Category Added

--- app/Photo.php
+++ app/Photo.php
@@ -9,6 +9,7 @@ class Photo extends Model
     protected $uploads = '/images/';
     protected $fillable = ['file'];
 
+    
     public function getFileAttribute($photo){
     	
     	return $this->uploads . $photo;
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,10 @@
-<h1>Custom 404 Page</h1>
+@extends('layouts.app')
+
+
+@section('content')
+
+<div class="text-center">
+	<h1>Custom 404 Page</h1>	
+</div>
+
+@stop
